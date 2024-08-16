d02c099185717db799ab9490b6487580616901ff
yannickRadji <yannickRadji@users.noreply.github.com>
2024-08-16 16:55:15 +0200

    from snowflake to vanilla

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 from functools import reduce
 
@@ -11,7 +10,8 @@ st.write(
     """
 )
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
 st.dataframe(data=my_dataframe, use_container_width=True)
 
